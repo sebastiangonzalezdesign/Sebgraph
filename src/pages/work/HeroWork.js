2a9d51ca1786f107f0c6b49c74aa38d1d71a5df3
sebgraph <sebgraph7@gmail.com>
2023-02-21 18:35:11 -0500

    header and hero updates

--- src/pages/work/HeroWork.js
+++ src/pages/work/HeroWork.js
@@ -5,6 +5,7 @@ import ThemeContext from "../../context/ThemeContext";
 import Aos from "aos";
 import "aos/dist/aos.css";
 import React from "react";
+import { Button } from "../../components/Button";
 
 const HeroWork = () => {
   const { theme } = useContext(ThemeContext);
@@ -14,14 +15,37 @@ const HeroWork = () => {
   }, []);
   return (
     <section className={`section-hero ${theme}`}>
-      <article className="section-hero__text-container">
-        <h1 className="heading-primary" data-aos="fade-up" data-aos-delay="500">
-          Hi! I’m Sebastian
-        </h1>
-        <p className="paragraph">
-          I will help you to connect the users with your digital products
-          through great experiences and memorable interfaces.
-        </p>
+      <article className="section-hero__container">
+        <div className="section-hero__container__text-container">
+          <h1
+            className="display-100--bold"
+            data-aos="fade-up"
+            data-aos-delay="500"
+          >
+            Hi! I’m Sebastian
+          </h1>
+          <p className="paragraph">
+            I will help you to connect the users with your digital products
+            through great experiences and memorable interfaces.
+          </p>
+        </div>
+
+        <div className="section-hero__btn-container">
+          <Button
+            type="button"
+            buttonStyle="btn--secondary"
+            buttonSize="btn--large"
+          >
+            View work
+          </Button>
+          <Button
+            type="button"
+            buttonStyle="btn--primary"
+            buttonSize="btn--large"
+          >
+            Download CV
+          </Button>
+        </div>
       </article>
 
       <img
