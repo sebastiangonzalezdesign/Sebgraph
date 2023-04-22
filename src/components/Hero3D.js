56216914c83101ebddcab5125829056c0b4d8003
sebgraph <sebgraph7@gmail.com>
2023-04-22 18:47:09 -0500

    3d updated

--- src/components/Hero3D.js
+++ src/components/Hero3D.js
@@ -1,11 +1,16 @@
-import Spline from "@splinetool/react-spline";
+/* import Spline from "@splinetool/react-spline"; */
+import React, { Suspense } from "react";
 import "./Hero3D.scss";
+import Spinner from "./Spinner";
+const Spline = React.lazy(() => import("@splinetool/react-spline"));
 
 export default function Hero3D() {
   return (
-    <Spline
-      id="scene"
-      scene="https://prod.spline.design/pU1tFHuw9WdUrsdP/scene.splinecode"
-    />
+    <Suspense fallback={<Spinner />}>
+      <Spline
+        id="scene"
+        scene="https://prod.spline.design/pm1nUvLjlHNtgX4N/scene.splinecode"
+      />
+    </Suspense>
   );
 }
