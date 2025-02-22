b6b37400e5bd7fd0d940095613a8b3ae1e8e0e03
Sebastian Gonzalez <sebgraph7@gmail.com>
2025-02-22 17:11:09 -0500

    adjusted Error layout

--- src/pages/Error404.tsx
+++ src/pages/Error404.tsx
@@ -5,16 +5,26 @@ import './Error.scss'
 const Error404 = () => {
     return (
         <section className="error-section section-hero bg-white mt-40">
-            <figure className="img">
-                <img src={Error} alt="Error 404" />
+            <figure className="error-section__img">
+                <img
+                    className="error-section__img"
+                    src={Error}
+                    alt="Error 404"
+                />
             </figure>
-            <h1 className="heading-secondary">Ooops! Page not found</h1>
-            <p className="paragraph">
-                The page you are looking is not available
-            </p>
-            <a href="https://sebastiangonzalez.design/">
-                <Button>Back to home</Button>
-            </a>
+            <div className="error-section__content-box">
+                <div className="error-section__text-box">
+                    <h1 className="heading__100--bold">
+                        Ooops! Page not found
+                    </h1>
+                    <p className="paragraph__100--medium">
+                        The page you are looking is not available
+                    </p>
+                </div>
+                <a href="https://sebastiangonzalez.design/">
+                    <Button>Back to home</Button>
+                </a>
+            </div>
         </section>
     )
 }
